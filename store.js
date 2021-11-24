fffb1a940c2566ed99b5a7836b5349e767331140
Arshad Chowdhury <86738490+ArshadChowdhury@users.noreply.github.com>
2021-11-25 02:40:15 +0600

    Update store.js

--- store.js
+++ store.js
@@ -43,9 +43,8 @@ function quantityChanged(event) {
   var input = event.target;
   if (isNaN(input.value) || input.value <= 0) {
     input.value = 1;
-    updateCartTotal();
   }
-  
+  updateCartTotal();
 }
 
 function removeCartItems(event) {
